3d564e82c1fab6c84949b3a73de7daac0190cd83
valnech <valnech.m@gmail.com>
2012-12-07 10:56:24 +0200

    raills app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestParser::Application.config.secret_token = 'd3e82239ad3549205286e3b34852a40c7472f77b92a63ae3e89b6f46c1910c256a05ddd1b14723d71ed725bd5460bed0e1b84564688eed5a556af692ac0a632a'
